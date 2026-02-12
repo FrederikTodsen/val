cd30e39eb25c9b7cb62571d94e89380d783eacfd
Frederik Todsen <FrederikTodsen@hotmail.dk>
2026-02-12 23:59:22 +0100

    færdig

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const envelope = document.querySelector('.envelope-wrapper');
 const heart = document.querySelector('.heart');
 
-heart.addEventListener('mouseenter', () => {
-    envelope.classList.add('flap');
-});
-
-heart.addEventListener('mouseleave', () => {
-    envelope.classList.remove('flap');
+heart.addEventListener('click', () => {
+    // Hvis den er åben, lukker den. Hvis den er lukket, åbner den.
+    envelope.classList.toggle('flap');
 });
